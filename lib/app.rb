f42bafc7c9fec83f6ee1665f8f335aed93ecc8c5
Nathan Fisher <nfisher@junctionbox.ca>
2012-09-25 02:53:06 +0100

    @nfisher: Need to migrate null usernames first.

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,7 @@ class Person
   property :id,   Serial
   property :firstname, String, :required => true
   property :lastname, String,  :required => true
-  property :username, String,  :required => true
+  property :username, String
   property :uid,  Integer,     :required => true
 end
 
